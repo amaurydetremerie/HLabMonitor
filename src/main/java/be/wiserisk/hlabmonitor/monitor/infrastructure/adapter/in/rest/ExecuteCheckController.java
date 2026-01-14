0f29130f8f8a76204e7165e2aa7af593318bbb24
adetremerie <adetremerie@paradigm.brussels>
2026-01-14 15:52:42 +0100

    Add debug controller

---
+++ src/main/java/be/wiserisk/hlabmonitor/monitor/infrastructure/adapter/in/rest/ExecuteCheckController.java
@@ -0,0 +1,27 @@
+package be.wiserisk.hlabmonitor.monitor.infrastructure.adapter.in.rest;
+
+import be.wiserisk.hlabmonitor.monitor.application.port.in.execution.ExecuteCheckUseCase;
+import be.wiserisk.hlabmonitor.monitor.domain.model.TargetId;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/debug/execute")
+@AllArgsConstructor
+@Tag(name = "Execute a check (debug only)")
+public class ExecuteCheckController {
+
+    private ExecuteCheckUseCase executeCheckUseCase;
+
+    @Operation(summary = "Execute the check for a Target")
+    @PostMapping("/{targetId}")
+    public ResponseEntity<Void> executeByTargetId(@PathVariable String targetId) {
+        executeCheckUseCase.executeCheck(new TargetId(targetId));
+        return ResponseEntity.accepted().build();
+    }
+
+}
