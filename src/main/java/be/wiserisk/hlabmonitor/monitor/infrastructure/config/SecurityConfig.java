152eb11ae56dbd74ada4ee9a5354e9ebd14e126e
adetremerie <adetremerie@paradigm.brussels>
2026-02-09 11:59:30 +0100

    Add actuator config

--- src/main/java/be/wiserisk/hlabmonitor/monitor/infrastructure/config/SecurityConfig.java
+++ src/main/java/be/wiserisk/hlabmonitor/monitor/infrastructure/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
                         .requestMatchers("/api/**").permitAll()
                         .requestMatchers("/v3/api-docs.yaml", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
