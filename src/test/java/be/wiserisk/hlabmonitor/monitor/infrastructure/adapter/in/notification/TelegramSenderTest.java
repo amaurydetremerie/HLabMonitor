d69b5004feef0d9c10adbbda20750fa0b732ff2d
amaurydetremerie <amaury.detremerie@gmail.com>
2026-03-05 14:30:54 +0100

    Add tests

--- src/test/java/be/wiserisk/hlabmonitor/monitor/infrastructure/adapter/in/notification/TelegramSenderTest.java
+++ src/test/java/be/wiserisk/hlabmonitor/monitor/infrastructure/adapter/in/notification/TelegramSenderTest.java
@@ -12,7 +12,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.MockedConstruction;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,47 +39,80 @@ class TelegramSenderTest {
 
     @Test
     void sendNotification_SEND() throws JsonProcessingException {
-        ObjectMapper objectMapper = mock(ObjectMapper.class);
-        Notification notification = mock(Notification.class);
-        when(notification.notificationStatus()).thenReturn(NotificationStatus.SEND);
-        when(notification.targetId()).thenReturn(new TargetId("targetId"));
-        TelegramSender telegramSender = new TelegramSender(objectMapper, "", "");
-        ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
-        assertDoesNotThrow(() -> telegramSender.sendNotification(notification));
-        verify(objectMapper, times(1)).writeValueAsBytes(captor.capture());
-        assertThat(captor.getValue()).hasSize(3)
-                .extracting("text").isNotNull()
-                .matches(s -> s.toString().contains("[FIRING]"));
+        try(MockedConstruction<URI> uriMockedConstruction = mockConstruction(URI.class, (mock, ctx) -> {
+            URL url = mock(URL.class);
+            HttpsURLConnection httpsURLConnection = mock(HttpsURLConnection.class);
+            OutputStream outputStream = mock(OutputStream.class);
+            InputStream inputStream = mock(InputStream.class);
+            when(mock.toURL()).thenReturn(url);
+            when(url.openConnection()).thenReturn(httpsURLConnection);
+            when(httpsURLConnection.getOutputStream()).thenReturn(outputStream);
+            when(httpsURLConnection.getInputStream()).thenReturn(inputStream);
+        })) {
+            ObjectMapper objectMapper = mock(ObjectMapper.class);
+            Notification notification = mock(Notification.class);
+            when(notification.notificationStatus()).thenReturn(NotificationStatus.SEND);
+            when(notification.targetId()).thenReturn(new TargetId("targetId"));
+            TelegramSender telegramSender = new TelegramSender(objectMapper, "", "");
+            ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
+            when(objectMapper.writeValueAsBytes(captor.capture())).thenReturn(new byte[0]);
+            assertDoesNotThrow(() -> telegramSender.sendNotification(notification));
+            assertThat(captor.getValue()).hasSize(3)
+                    .extracting("text").isNotNull()
+                    .matches(s -> s.toString().contains("[FIRING]"));
+        }
     }
 
     @Test
     void sendNotification_TERMINATED() throws JsonProcessingException {
-        ObjectMapper objectMapper = mock(ObjectMapper.class);
-        Notification notification = mock(Notification.class);
-        when(notification.notificationStatus()).thenReturn(NotificationStatus.TERMINATED);
-        when(notification.targetId()).thenReturn(new TargetId("targetId"));
-        TelegramSender telegramSender = new TelegramSender(objectMapper, "", "");
-        ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
-        assertDoesNotThrow(() -> telegramSender.sendNotification(notification));
-        verify(objectMapper, times(1)).writeValueAsBytes(captor.capture());
-        assertThat(captor.getValue()).hasSize(3)
-                .extracting("text").isNotNull()
-                .matches(s -> s.toString().contains("[RESOLVED]"));
+        try(MockedConstruction<URI> uriMockedConstruction = mockConstruction(URI.class, (mock, ctx) -> {
+            URL url = mock(URL.class);
+            HttpsURLConnection httpsURLConnection = mock(HttpsURLConnection.class);
+            OutputStream outputStream = mock(OutputStream.class);
+            InputStream inputStream = mock(InputStream.class);
+            when(mock.toURL()).thenReturn(url);
+            when(url.openConnection()).thenReturn(httpsURLConnection);
+            when(httpsURLConnection.getOutputStream()).thenReturn(outputStream);
+            when(httpsURLConnection.getInputStream()).thenReturn(inputStream);
+        })) {
+            ObjectMapper objectMapper = mock(ObjectMapper.class);
+            Notification notification = mock(Notification.class);
+            when(notification.notificationStatus()).thenReturn(NotificationStatus.TERMINATED);
+            when(notification.targetId()).thenReturn(new TargetId("targetId"));
+            TelegramSender telegramSender = new TelegramSender(objectMapper, "", "");
+            ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
+            when(objectMapper.writeValueAsBytes(captor.capture())).thenReturn(new byte[0]);
+            assertDoesNotThrow(() -> telegramSender.sendNotification(notification));
+            assertThat(captor.getValue()).hasSize(3)
+                    .extracting("text").isNotNull()
+                    .matches(s -> s.toString().contains("[RESOLVED]"));
+        }
     }
 
     @Test
     void sendNotification_FAILED() throws JsonProcessingException {
-        ObjectMapper objectMapper = mock(ObjectMapper.class);
-        Notification notification = mock(Notification.class);
-        when(notification.notificationStatus()).thenReturn(NotificationStatus.FAILED);
-        when(notification.targetId()).thenReturn(new TargetId("targetId"));
-        TelegramSender telegramSender = new TelegramSender(objectMapper, "", "");
-        ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
-        assertDoesNotThrow(() -> telegramSender.sendNotification(notification));
-        verify(objectMapper, times(1)).writeValueAsBytes(captor.capture());
-        assertThat(captor.getValue()).hasSize(3)
-                .extracting("text").isNotNull()
-                .matches(s -> s.toString().contains("[FAILED]"));
+        try(MockedConstruction<URI> uriMockedConstruction = mockConstruction(URI.class, (mock, ctx) -> {
+            URL url = mock(URL.class);
+            HttpsURLConnection httpsURLConnection = mock(HttpsURLConnection.class);
+            OutputStream outputStream = mock(OutputStream.class);
+            InputStream inputStream = mock(InputStream.class);
+            when(mock.toURL()).thenReturn(url);
+            when(url.openConnection()).thenReturn(httpsURLConnection);
+            when(httpsURLConnection.getOutputStream()).thenReturn(outputStream);
+            when(httpsURLConnection.getInputStream()).thenReturn(inputStream);
+        })) {
+            ObjectMapper objectMapper = mock(ObjectMapper.class);
+            Notification notification = mock(Notification.class);
+            when(notification.notificationStatus()).thenReturn(NotificationStatus.FAILED);
+            when(notification.targetId()).thenReturn(new TargetId("targetId"));
+            TelegramSender telegramSender = new TelegramSender(objectMapper, "", "");
+            ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
+            when(objectMapper.writeValueAsBytes(captor.capture())).thenReturn(new byte[0]);
+            assertDoesNotThrow(() -> telegramSender.sendNotification(notification));
+            assertThat(captor.getValue()).hasSize(3)
+                    .extracting("text").isNotNull()
+                    .matches(s -> s.toString().contains("[FAILED]"));
+        }
     }
 
 }
